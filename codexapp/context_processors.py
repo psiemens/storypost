75d5c9eaba7c891dab3f3a49dc6fb9629e450999
Peter Siemens <peterjsiemens@gmail.com>
2015-06-28 01:18:06 -0700

    add error handling to user context processor

--- codexapp/context_processors.py
+++ codexapp/context_processors.py
@@ -1,8 +1,11 @@
 from codexapp.main.models import User
 
 def user_id(request):
-    user = User.objects.get(auth_user=request.user.id)
-    return user.id
+    try:
+        user = User.objects.get(auth_user=request.user.id)
+        return user.id
+    except:
+        return None
 
 def user(request):
     return {
