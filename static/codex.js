bb7516ef47390f11af6c9a0f8890d6e17edfe973
Ben Cook <b@bencook.ca>
2015-06-28 12:03:00 -0700

    fix 2000 years ago bug :(

--- static/codex.js
+++ static/codex.js
@@ -1,6 +1,8 @@
 !(function(self) {
     self.timeparse = function(s) {
-        return moment(s.replace(".", ""), "MMM D, YYYY, h:mm a")
+        if (s.startsWith("2015"))  // #TODO wtf
+            return moment(s, "YYYY-MM-DD")
+        else return moment(s.replace(".", ""), "MMM D, YYYY, h:mm a")
     }
     self.relativise_timestamps = function() {
         $(".human-timestamp").each(function(idx, el) {
