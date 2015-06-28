726e8731328a0d48ea779de6a727d0cd9dd54244
Peter Siemens <peterjsiemens@gmail.com>
2015-06-28 12:49:20 -0700

    change timeparse

--- static/codex.js
+++ static/codex.js
@@ -2,8 +2,6 @@
     self.timeparse = function(s, format) {
         if(typeof format == 'undefined')
             format = "MMM D, YYYY, h:mm a";
-        if (s.startsWith("2015"))  // #TODO wtf
-            return moment(s, "YYYY-MM-DD")
         else return moment(s.replace(".", ""), format)
     }
     self.relativise_timestamps = function() {
